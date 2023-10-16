7d6b61b9f4cac8439f550e4cc7a14f882f2eeacb
Mnagireddy1234 <94007672+Mnagireddy1234@users.noreply.github.com>
2023-10-16 12:29:01 +0530

    Update StringReverse.js

--- StringReverse.js
+++ StringReverse.js
@@ -1,3 +1,8 @@
+JavaScript.
+1.Take a sentence as an input and reverse every word in that sentence. 
+   Example - This is a sunny day > shiT si a ynnus yad.
+	
+Solution:	
 let string="This is a sunny day";
 let revstr=string =>{
 	let str=string.split(" ");
@@ -7,3 +12,15 @@ let revstr=string =>{
 	return rev.join(" ");
 }
 document.write(revstr(string));
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<script type="text/javascript" src="StringReverse.js"></script>
+</head>
+<body>
+
+</body>
+</html>
