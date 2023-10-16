12e633cbd1a319d8f338029e19b555b8bf5cd810
Mnagireddy1234 <94007672+Mnagireddy1234@users.noreply.github.com>
2023-10-16 12:38:51 +0530

    Update SortingArray.js

--- SortingArray.js
+++ SortingArray.js
@@ -1,3 +1,8 @@
+JavaScript.
+  
+2.Perform sorting of an array in descending order.
+  
+solution:  
 let flowers=["Rose","Jasmine","lotus","Daisy"];
 flowers.sort((a,b)=>a.localeCompare(b));
 flowers.reverse();
@@ -9,3 +14,15 @@ document.write("<br>");
 let number=[12,1,32,44,33];
 number.sort((a,b)=>b-a);
 document.write(number);
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<script type="text/javascript" src="SortingArray.js"></script>
+</head>
+<body>
+
+</body>
+</html>
